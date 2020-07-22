055ecab9cee10e6342d490849932def2dc0aa6a3
Alex Costan <acostan.con@orangetheory.com>
2020-07-22 16:56:20 -0400

    using pillows filter methods on images

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -9,13 +9,25 @@ print(img.mode)  # RGB
 print(dir(img))  # displays every method we can apply to img
 
 blurred_image = img.filter(ImageFilter.BLUR)
-blurred_image.save("blur.png", 'png')
+blurred_image.save("./Filtered Images/blur.png", 'png')
 
 smooth_image = img.filter(ImageFilter.SMOOTH)  # Mainly used for landscape images
-smooth_image.save("smooth.png", 'png')
+smooth_image.save("./Filtered Images/smooth.png", 'png')
 
 sharpen_image = img.filter(ImageFilter.SHARPEN)
-sharpen_image.save('sharpened.png', 'png')
+sharpen_image.save('./Filtered Images/sharpened.png', 'png')
 
 converted_image = img.convert('L')
-converted_image.save('grey.png', 'png')
+converted_image.save('./Filtered Images/grey.png', 'png')
+converted_image.show()  # displays the image on your systems default viewer
+
+crooked = converted_image.rotate(90)
+crooked.save('./Filtered Images/crooked.png', 'png')
+
+resize = converted_image.resize((300, 300))
+resize.save('./Filtered Images/resized.png', 'png')
+
+# Cropping Images
+box = (100, 100, 400, 400)
+region = img.crop(box)
+region.save('./Filtered Images/cropped.png', 'png')
