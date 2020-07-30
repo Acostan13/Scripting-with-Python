518771858594360ea7f7049011774e39d90cfab3
Alex Costan <acostan.con@orangetheory.com>
2020-07-30 16:30:26 -0400

    BeatifulSoup Selectors

--- scrape.py
+++ scrape.py
@@ -10,3 +10,10 @@ print(soup.find_all('div'))  # gives a list of all the div's on the page
 print(soup.title)  # <title>Hacker News</title>
 print(soup.a)  # gives first <a> tag on the page
 print(soup.find(id='score_23995750'))  # <span class="score" id="score_23995750">1364 points</span>
+print(soup.select('.score'))  # grabs all the span tags with class '.score'
+print(soup.select('#score_23995750'))  # grabs all the span tags with id 'score_23995750'
+
+links = soup.select('.storylink')
+votes = soup.select('.score')
+print(votes[0])  # <span class="score" id="score_23999542">181 points</span>
+votes.get('id')  # score_23999542
