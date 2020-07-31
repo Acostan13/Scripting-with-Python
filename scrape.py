b9821aa13d13ab83b36804b847c229db0fec4dce
Alex Costan <acostan.con@orangetheory.com>
2020-07-31 11:12:22 -0400

    scraping articles with over 99 votes

--- scrape.py
+++ scrape.py
@@ -1,33 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+# print(res.text)
 
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup.body.contents) # gives the entire html content of the page
-print(soup.find_all('div'))  # gives a list of all the div's on the page
-print(soup.title)  # <title>Hacker News</title>
-print(soup.a)  # gives first <a> tag on the page
-print(soup.find(id='score_23995750'))  # <span class="score" id="score_23995750">1364 points</span>
-print(soup.select('.score'))  # grabs all the span tags with class '.score'
-print(soup.select('#score_23995750'))  # grabs all the span tags with id 'score_23995750'
+# print(soup.body.contents) # gives the entire html content of the page
+# print(soup.find_all('div'))  # gives a list of all the div's on the page
+# print(soup.title)  # <title>Hacker News</title>
+# print(soup.a)  # gives first <a> tag on the page
+# print(soup.find(id='score_23995750'))  # <span class="score" id="score_23995750">1364 points</span>
+# print(soup.select('.score'))  # grabs all the span tags with class '.score'
+# print(soup.select('#score_23995750'))  # grabs all the span tags with id 'score_23995750'
 
 links = soup.select('.storylink')
-votes = soup.select('.score')
-print(votes[0])  # <span class="score" id="score_23999542">181 points</span>
-votes[0].get('id')  # score_23999542
+subtext = soup.select('.subtext')
+print(subtext[0])  # <span class="score" id="score_23999542">181 points</span>
+subtext[0].get('id')  # score_23999542
 
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for ind, item in enumerate(links):
-        title = links[ind].getText()
-        href = links[ind].get('href', None)
-        points = int(votes[ind].getText().replace(' points', ''))
-        print(points)
-        hn.append({'title': title, 'link': href})
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[ind].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:  
+                # print(points)
+                hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
 
-print(create_custom_hn(links, votes))  # displays all the links on the page
+pprint.pprint(create_custom_hn(links, subtext))  # displays all the links on the page
+
